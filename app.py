260c4d6d4c9ebab7afcd41a9f5606619e0b7d191
Jaydarian <jayrey.go@gmail.com>
2019-01-28 00:07:02 -0600

    convert while statements to functions
    We have arrived a stop due to being unable to return my input in the get_date function.  Next is making the get_notes functino to be operational

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import os
 
 from datetime import datetime
 
+
 def clear_screen():
 	os.system("cls" if os.name == "nt" else "clear")
 
+# Optimize following the route to add a journal
 def new_page(letter_choice):
     clear_screen()
     if letter_choice.lower() == "a":
@@ -15,8 +17,54 @@ def new_page(letter_choice):
     if letter_choice == "c":
         pass
 
+# Continuously ask the user to add date
+def get_date_from_user():
+    while True: 
+        try:  # Give the date
+            print( "Date of the task\nPlease use DD/MM/YYYY\n")
+            date_input = input("> ")
+            # Ensure that date format is valid
+            parsed_date = datetime.strptime(date_input, '%m/%d/%Y')
+            clear_screen()
+            return date_input
+            break
+        except ValueError:
+            print("\n{} doesn't seem to be a valid date and time."
+                  .format(date_input))
+            os.system("pause")
+            clear_screen()
+            continue
+    return date_input
+
+# Contrinuously ask the user to add title
+def get_title_from_user():
+    while True:
+        try:  # Give title of task
+            print("Title of the task\n")
+            title_input = input("> ")
+            if len(title_input) == 0:
+                raise NameError("Please enter a valid title.")
+        except NameError as e:
+            print(e)
+            os.system("pause")
+            clear_screen()
+            continue
+        clear_screen()
+        break
+
+def get_time_from_user():
+    while True:
+        #try:  # Give time spent on task
+        print("Time spent on the task (rounded)\n")
+        time_spent_value = input("> ")
+        time_spent_value = int(time_spent_value)
+    
+
+        break
 
 worklog = {}
+
+
 clear_screen()
 print(
     "== WORK LOG==\n"
@@ -25,41 +73,31 @@ print(
     "b) Search existing entries\n"
     "c) Quit program\n"
 )
-
 choice = input("> ")
-
 clear_screen()
 
 # Follow the 'Add new entry' route
 while choice.lower() == "a":
     
-    try:  # Give the date
-        print( "Date of the task\nPlease use DD/MM/YYYY\n")
-        date_input = input("> ")
-        # Ensure that date format is valid
-        parsed_date = datetime.strptime(date_input, '%m/%d/%Y')
+    get_date_from_user()
+
+    get_title_from_user()
+    
+    worklog.update([
+    ('Date', date_input), 
+    ('Title', title_input),
+    ('Time Spent', time_spent_value),
+    ('Notes', notes_value)
+    ])
+
+    print(worklog)
+    
+
+
+    break  # the "Add new Entry route"
         
-    except ValueError:
-        print("\n{} doesn't seem to be a valid date and time."
-              .format(date_input))
-        os.system("pause")
-        clear_screen()
-    else:
-        break
-    
-            
-    
 clear_screen()
 
-# Give title of task
-print("Title of the task\n")
-title_input = input("> ")
-clear_screen()
-
-# Give time spent on task
-print("Time spent on the task (rounded)\n")
-time_spent_value = input("> ")
-clear_screen()
 
 # Give optional notes of task
 print("Notes (optional)\n")
